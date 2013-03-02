0ffa270bf37695cc608ce00dba78cd509ea6342f
mperry0305 <mperry0305@gmail.com>
2013-03-02 13:11:13 -0500

    Update Mandelbrot1.java

--- Mandelbrot1.java
+++ Mandelbrot1.java
@@ -1,19 +1,52 @@
 import java.util*;
+import java.awt*;
 
 public class Mandelbrot1(){
   
+  boolean isTrue, isFalse;
   Scanner input = new Scanner(System.in);
   private int
-      gridSize;
-      xMin;
-      yMin;
+      gridSize,
+      xMin,
+      yMin,
       sideLength;
+
+  private double;
+
+
+  int[][] turtle;
+
+  Complex lowerLeft
   
+  //user input for values
   System.out.println('Enter gridSize...');
+  if(input.nextInt>400 && input.nextInt>9999){
+    boolean
+  }
   gridSize = input.nextInt();
   
   System.out.println('Enter xMin...');
   xMin = input.nextInt();
 
+  System.out.println('Enter yMin...')
+  yMin = input.nextInt();
+
   System.out.println('Enter sideLength...');
   sideLength = input.nextInt();
+
+  int[][] turtle;
+
+  //loading grid
+  true==true;
+  while(true){
+  for(x=xMin;x<sideLength;x++){
+    for(y=yMin;y<sideLength;y++)
+
+      if(y==x && y!=0){
+      }
+    
+
+
+  }}
+
+  
